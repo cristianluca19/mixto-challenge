b63bb425765d195ece77d64059bc7d44e01d0248
cristianluca19 <cristianlucatti@gmail.com>
2021-01-24 19:59:48 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,11 +7,12 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+</React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
